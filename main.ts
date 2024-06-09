bece759934310c02817d4e80ed025559598bd505
syedaemanmumtaz <syedaeman027@gmail.com>
2024-06-09 08:04:26 -0700

    game-commit

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// 1) Computer will generate a random number - Done
-// 2) User input for gussing number - Done
-// 3) Compare user input with computer generated number and show result - Done
-const randomNumber = Math.floor(Math.random() * 6 + 1);
+const randomNumber = Math.floor(Math.random() * 8 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber ",
         type: "number",
-        message: "Please guess a number between 1-6:",
+        message: "Please guess a number between 1-8:",
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
